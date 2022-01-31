0af5c7a31d32b44b504546fea0f13c31f23c5bb9
dymafr <contact@dyma.fr>
2022-01-31 13:51:09 +0100

    fix: s6

--- src/Service/Uploader.php
+++ src/Service/Uploader.php
@@ -2,26 +2,25 @@
 
 namespace App\Service;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class Uploader
 {
 
-    public function __construct(private ContainerInterface $container, private Filesystem $fs)
+    public function __construct(private Filesystem $fs,  private $profileFolder, private $profileFolderPublic)
     {
     }
 
     public function uploadProfileImage(UploadedFile $picture, string $oldPicturePath = null): string
     {
-        $folder = $this->container->getParameter('profile.folder');
+        $folder = $this->profileFolder;
         $ext = $picture->guessExtension() ?? 'bin';
         $filename = bin2hex(random_bytes(10)) . '.' . $ext;
         $picture->move($folder, $filename);
         if ($oldPicturePath) {
             $this->fs->remove($folder . '/' . pathinfo($oldPicturePath, PATHINFO_BASENAME));
         }
-        return $this->container->getParameter('profile.folder.public_path') . '/' . $filename;
+        return $this->profileFolderPublic . '/' . $filename;
     }
 }
